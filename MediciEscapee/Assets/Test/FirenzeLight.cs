3623d32a54fc1d910b2aeb706b5a5571f25dfc34
heyou32 <77083184+heyou32@users.noreply.github.com>
2021-02-25 19:01:51 +0900

    0225 SteamVR 에셋, 피렌체 스위치

---
+++ MediciEscapee/Assets/Test/FirenzeLight.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirenzeLight : MonoBehaviour
+{
+    public GameObject dLight;
+    public GameObject spotLight;
+    public bool trigger;
+    bool isOn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dLight.SetActive(false);
+        spotLight.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (trigger)
+        //{
+        //    dLight.SetActive(true);
+        //    spotLight.SetActive(false);
+        //}
+        //else
+        //{
+        //    dLight.SetActive(false);
+        //    spotLight.SetActive(true);
+        //}
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        isOn = !isOn;
+        if (other != null)
+        {
+            if (isOn)
+            {
+                dLight.SetActive(true);
+                spotLight.SetActive(false);
+            }
+            if (!isOn)
+            {
+                dLight.SetActive(false);
+                spotLight.SetActive(true);
+            }
+        }
+    }
+}
---
+++ MediciEscapee/Assets/Test/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Transform startPoint;
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+
+    }
+    internal void Ready()
+    {
+        //SFX
+        Invoke("StartPoint",5);
+    }
+    void StartPoint()
+    {
+        transform.position = startPoint.position;
+    }
+}
