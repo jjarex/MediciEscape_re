029db141dee3edab8689e9705b02a7311de5ac95
heyou32 <77083184+heyou32@users.noreply.github.com>
2021-02-26 15:18:45 +0900

    playermove script 추가

---
+++ MediciEscapee/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class PlayerMove : MonoBehaviour
+{
+    public SteamVR_Action_Vector2 move;
+    public float speed = 5;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Vector2 dir2 = move.GetAxis(SteamVR_Input_Sources.LeftHand);
+        float movex = dir2.x;
+        float movey = dir2.y;
+        Vector3 dir3 = new Vector3(movex, 0, movey);
+        dir3 = Camera.main.transform.TransformDirection(dir3);
+        dir3.y = 0;
+        transform.position += dir3 * Time.deltaTime * speed;
+    }
+}
