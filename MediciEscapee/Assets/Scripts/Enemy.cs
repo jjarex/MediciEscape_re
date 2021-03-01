9a7ace3760bfd6393c9204e21a00a7bcb60ba073
heyou32 <77083184+heyou32@users.noreply.github.com>
2021-03-01 10:04:45 +0900

    0301 에너미 추가, 플레이어 트리거 수정, 게임매니저 추가
    수정 중

--- MediciEscapee/Assets/Scripts/Enemy.cs
+++ MediciEscapee/Assets/Scripts/Enemy.cs
@@ -7,52 +7,86 @@ using UnityEngine.AI;
 public class Enemy : MonoBehaviour
 {
     NavMeshAgent agent;
-    void Start()
-    {
-        agent = GetComponent<NavMeshAgent>();
-    }
+    public GameObject player;
+    public Transform destinaion;
+    public float chaseSpeed=100;
     public enum State
     {
-        Search,
-        Move,
-        Test,
+        Patrol,
+        Chase,
+        Quiz,
         Happy
     }
     public State state;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        state = State.Patrol;
+    }
 
     void Update()
     {
         switch (state)
         {
-            case State.Search:
-                UpdateSearch(); break;
-            case State.Move:
-                UpdateMove(); break;
-            case State.Test:
-                UpdateTest(); break;
+            case State.Patrol:
+                UpdatePatrol(); break;
+            case State.Chase:
+                UpdateChase(); break;
+            case State.Quiz:
+                UpdateQuiz(); break;
             case State.Happy:
                 UpdateHappy(); break;
         }
-        if (state == State.Test)
+        if (state == State.Quiz)
             Timer.instance.pause = true;
         else
             Timer.instance.pause = false;
     }
 
-    private void UpdateSearch()
+    private void UpdatePatrol()
     {
+        //int r = UnityEngine.Random.Range(0, wayPoints.Length);
+        //float distance = Vector3.Distance(transform.position, agent.transform.position);
+        //if (distance <= agent.stoppingDistance)
+        //{
+        //    r++;
+        //    if (distance == wayPoints.Length)
+        //        agent.destination = wayPoints[r].position;
+        //    r = 0;
+        //}
+        agent.destination = destinaion.transform.position;
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hitinfo;
+        if (Physics.Raycast(ray, out hitinfo))
+        {
+            if (hitinfo.transform == player)
+            {
+                state = State.Chase;
+            }
+        }
     }
 
-    private void UpdateMove()
+    private void UpdateChase()
     {
+        agent.destination = player.transform.position;
+        agent.speed *= chaseSpeed;
+
+        float distance = Vector3.Distance(transform.position, player.transform.position);
+        if (distance<=agent.stoppingDistance)
+        {
+            state = State.Quiz;
+        }
     }
 
-    private void UpdateTest()
+    private void UpdateQuiz()
     {
+        player.GetComponent<PlayerMove>().catched = true;
     }
 
     private void UpdateHappy()
     {
         Timer.instance.currentTime += 30;
+
+        Destroy(gameObject, 5);
     }
 }
