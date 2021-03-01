80489c398f31ea72d5be56655430a18d12b2be4f
hjjr2280 <78453942+hjjr2280@users.noreply.github.com>
2021-03-01 20:33:11 +0900

    UI

---
+++ MediciEscapee/Assets/Scripts/Question.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Question : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    //    당신이 여기 갇힌 이유는?
+    //    1. 여기 살고 싶어서
+    //    2. 열심히 하는 모습 보이려고
+    //    3. 잠들어서
+    //    4. 귀신 만나려고
+    //    5. 아무생각이 없다
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
