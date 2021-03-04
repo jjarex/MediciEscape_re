cbee9873b041832072f73b8acbad0f4c32e8a6ec
heyou32 <77083184+heyou32@users.noreply.github.com>
2021-03-04 17:17:04 +0900

    0304.02

--- MediciEscapee/Assets/Scripts/EnemyPeekabooTrigger.cs
+++ MediciEscapee/Assets/Scripts/EnemyPeekabooTrigger.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 
 public class EnemyPeekabooTrigger : MonoBehaviour
 {
-    public GameObject peekabooEnemy;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            //에님&피카부에너미 디스트로이
-            Destroy(peekabooEnemy.gameObject, 2);
+            //에님
+            Destroy(transform.parent, 2);
         }
     }
-    
+
 }
